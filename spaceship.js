c2dc282f2d5057853efe0cc52ad0451f0e133d2f
github username <github email address>
2017-05-18 23:10:58 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,19 @@
+class Spaceship {
+  constructor(name, crew) {
+    this.name = name
+    this.crew = crew
+    this.phasers = 5
+    this.shields = 4
+    this.warpDrive = "disengaged"
+    this.cloaked = false
+    this.phasersCharge = "uncharged"
+      if (this.crew.length === 0) {
+        this.docked = true
+      } else {
+        this.docked = false
+        this.crew.forEach(function(member) {
+          member.currentShip = this
+        }, this)
+      }
+    }
+  }
